bc81c610d43ba76e46f2c4af062ad8f8189b782f
Parsankamusa <parsankamusa@gmail.com>
2025-09-22 23:11:57 +0300

    initial commit

--- frontend/src/pages/professional/profile/components/RequiredAsterisk.jsx
+++
@@ -1,1 +0,0 @@
-export const RequiredAsterisk = () => <span className="text-red-600">*</span>;
--- frontend/src/pages/recruiter/components/dropDownData.js
+++
@@ -1,7 +0,0 @@
-
-export const organizationTypes = [
-    { label: 'Company', value: 'company'},
-    { label: 'NGO', value: 'ngo'},
-    { label: 'Government', value: 'government'},
-    
-]
